60c0c87584709ab14d2a7db34f1add24df93a52f
Akira Hayakawa <ruby.wktk@gmail.com>
2015-11-01 12:54:28 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/functional/s3/Server.scala
+++ src/main/scala/functional/s3/Server.scala
@@ -1,44 +1,26 @@
 package functional.s3
 
 import akka.http.scaladsl.model.headers.RawHeader
-import akka.http.scaladsl.model.{HttpHeader, HttpRequest, StatusCodes}
+import akka.http.scaladsl.model.{HttpRequest, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.ExceptionHandler
-import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
 
-import scala.util.Random
 import scala.collection.immutable
+import scala.util.Random
 
-case class Server(config: ServerConfig) {
+trait RouteUtil {
   val extractBucket = path(Segment ~ (Slash | PathEnd))
   val extractObject = path(Segment / Rest)
+  val toRawHeader = (a: (String, String)) => RawHeader(a._1, a._2)
+}
 
+case class Server(config: ServerConfig) extends RouteUtil {
   def doOptionsObject(req: HttpRequest, reqId: String) = complete("hoge")
   def doPostObject(req: HttpRequest, reqId: String) = complete("hoge")
-  def doGetService() = complete("hoge")
-  def doGetBucketLocation(bucketName: String) = complete("hoge")
-  def doGetBucket(bucketName: String) = complete("hoge")
-  def doListParts(bucketName: String, keyName: String, uploadId: String) = complete("hoge")
-  def doListMultipartUploads(bucketName: String) = complete("hoge")
-  def doGetObject(bucketName: String, keyName: String) = complete("hoge")
-  def doPutBucket(bucketName: String) = complete("hoge")
-  def doUploadPart(bucketName: String, keyName: String, partNumber: Int, uploadId: String) = complete("hoge")
-  def doUploadPartByCopy(bucketName: String, keyName: String) = complete("hoge")
-  def doPutObject(bucketName: String, keyName: String) = complete("hoge")
-  def doDeleteBucket(bucketName: String) = complete("hoge")
-  def doAbortMultipartUpload(bucketName: String, keyName: String) = complete("hoge")
-  def doDeleteObject(bucketName: String, keyName: String) = complete("hoge")
-  def doDeleteMultipleObjects(bucketName: String) = complete("hoge")
-  def doInitiateMultipartUpload(bucketName: String, keyName: String) = complete("hoge")
-  def doCompleteMultipleUpload(bucketName: String, keyName: String, uploadId: String) = complete("hoge")
-  def doHeadBucket(bucketName: String) = complete("hoge")
-  def doHeadObject(bucketName: String, keyName: String) = complete("hoge")
 
+
+  val tree = Tree(config.treePath)
   val users = UserTable(config.adminPath)
-
-  val toRawHeader = (a: (String, String)) => RawHeader(a._1, a._2)
-
-  case class AuthorizedContext(callerId: Option[String], requestId: String)
 
   def handler(req: HttpRequest, requestId: String) = ExceptionHandler {
     // FIXME error should sometimes contains header info such as x-amz-delete-marker
@@ -71,118 +53,6 @@ case class Server(config: ServerConfig) {
         Error.mkXML(o, req.uri.path.toString(), requestId))
     }
   }
-
-  val normalRoute =
-    get {
-      path("") {
-        doGetService()
-      }
-    } ~
-    get {
-      extractBucket { bucketName =>
-        parameter("location") { a =>
-          doGetBucketLocation(bucketName)
-        }
-      }
-    } ~
-    get {
-      extractBucket { bucketName =>
-        doGetBucket(bucketName)
-      }
-    } ~
-    get {
-      extractObject { (bucketName, keyName) =>
-        parameter("uploadId") { uploadId =>
-          doListParts(bucketName, keyName, uploadId)
-        }
-      }
-    } ~
-    get {
-      extractBucket { bucketName =>
-        parameter("uploads") { a =>
-          doListMultipartUploads(bucketName)
-        }
-      }
-    } ~
-    get {
-      extractObject { (bucketName, keyName) =>
-        doGetObject(bucketName, keyName)
-      }
-    } ~
-    put {
-      extractBucket { bucketName =>
-        doPutBucket(bucketName)
-      }
-    } ~
-    put {
-      extractObject { (bucketName, keyName) =>
-        parameters("partNumber".as[Int], "uploadId".as[String]) { (partNumber, uploadId) =>
-          doUploadPart(bucketName, keyName, partNumber, uploadId)
-        }
-      }
-    } ~
-    put {
-      extractObject { (bucketName, keyName) =>
-        parameters("partNumber".as[Int], "uploadId".as[String]) { (partNumber, uploadId) =>
-          headerValueByName("x-amz-copy-source") { copySource => // /bucketName/key
-            doUploadPartByCopy(bucketName, keyName)
-          }
-        }
-      }
-    } ~
-    put {
-      extractObject { (bucketName, keyName) =>
-        doPutObject(bucketName, keyName)
-      }
-    } ~
-    delete {
-      extractBucket { bucketName =>
-        doDeleteBucket(bucketName)
-      }
-    } ~
-    delete {
-      extractObject { (bucketName, keyName) =>
-        parameter("uploadId") { a =>
-          doAbortMultipartUpload(bucketName, keyName)
-        }
-      }
-    } ~
-    delete {
-      extractObject { (bucketName, keyName) =>
-        doDeleteObject(bucketName, keyName)
-      }
-    } ~
-    post {
-      extractBucket { bucketName =>
-        parameter("delete") { a =>
-          doDeleteMultipleObjects(bucketName)
-        }
-      }
-    } ~
-    post {
-      extractObject { (bucketName, keyName) =>
-        parameter("uploads") { a =>
-          doInitiateMultipartUpload(bucketName, keyName)
-        }
-      }
-    } ~
-    post {
-      extractObject { (bucketName, keyName) =>
-        parameter("uploadId") { uploadId =>
-          doCompleteMultipleUpload(bucketName, keyName, uploadId)
-        }
-      }
-    } ~
-    head {
-      extractBucket { bucketName =>
-        doHeadBucket(bucketName)
-      }
-    } ~
-    head {
-      extractObject { (bucketName, keyName) =>
-        doHeadObject(bucketName, keyName)
-      }
-    }
 
   val route =
     extractRequest { req =>
@@ -219,10 +89,148 @@ case class Server(config: ServerConfig) {
               case (a, _) => a.flatMap { x => users.getId(x) }
             }
 
-            val context = AuthorizedContext(callerId, requestId)
-            normalRoute
+            val context = AuthorizedContext(tree, users, req, callerId, requestId)
+            context.route
           }
         }
       }
     }
 }
+
+  case class AuthorizedContext(tree: Tree,
+                               users: UserTable,
+                               req: HttpRequest,
+                               callerId: Option[String],
+                               requestId: String) extends RouteUtil
+  with GetService
+  {
+    def doGetBucketLocation(bucketName: String) = complete("hoge")
+    def doGetBucket(bucketName: String) = complete("hoge")
+    def doListParts(bucketName: String, keyName: String, uploadId: String) = complete("hoge")
+    def doListMultipartUploads(bucketName: String) = complete("hoge")
+    def doGetObject(bucketName: String, keyName: String) = complete("hoge")
+    def doPutBucket(bucketName: String) = complete("hoge")
+    def doUploadPart(bucketName: String, keyName: String, partNumber: Int, uploadId: String) = complete("hoge")
+    def doUploadPartByCopy(bucketName: String, keyName: String) = complete("hoge")
+    def doPutObject(bucketName: String, keyName: String) = complete("hoge")
+    def doDeleteBucket(bucketName: String) = complete("hoge")
+    def doAbortMultipartUpload(bucketName: String, keyName: String) = complete("hoge")
+    def doDeleteObject(bucketName: String, keyName: String) = complete("hoge")
+    def doDeleteMultipleObjects(bucketName: String) = complete("hoge")
+    def doInitiateMultipartUpload(bucketName: String, keyName: String) = complete("hoge")
+    def doCompleteMultipleUpload(bucketName: String, keyName: String, uploadId: String) = complete("hoge")
+    def doHeadBucket(bucketName: String) = complete("hoge")
+    def doHeadObject(bucketName: String, keyName: String) = complete("hoge")
+
+    val route =
+      get {
+        path("") {
+          doGetService()
+        }
+      } ~
+        get {
+          extractBucket { bucketName =>
+            parameter("location") { a =>
+              doGetBucketLocation(bucketName)
+            }
+          }
+        } ~
+        get {
+          extractBucket { bucketName =>
+            doGetBucket(bucketName)
+          }
+        } ~
+        get {
+          extractObject { (bucketName, keyName) =>
+            parameter("uploadId") { uploadId =>
+              doListParts(bucketName, keyName, uploadId)
+            }
+          }
+        } ~
+        get {
+          extractBucket { bucketName =>
+            parameter("uploads") { a =>
+              doListMultipartUploads(bucketName)
+            }
+          }
+        } ~
+        get {
+          extractObject { (bucketName, keyName) =>
+            doGetObject(bucketName, keyName)
+          }
+        } ~
+        put {
+          extractBucket { bucketName =>
+            doPutBucket(bucketName)
+          }
+        } ~
+        put {
+          extractObject { (bucketName, keyName) =>
+            parameters("partNumber".as[Int], "uploadId".as[String]) { (partNumber, uploadId) =>
+              doUploadPart(bucketName, keyName, partNumber, uploadId)
+            }
+          }
+        } ~
+        put {
+          extractObject { (bucketName, keyName) =>
+            parameters("partNumber".as[Int], "uploadId".as[String]) { (partNumber, uploadId) =>
+              headerValueByName("x-amz-copy-source") { copySource => // /bucketName/key
+                doUploadPartByCopy(bucketName, keyName)
+              }
+            }
+          }
+        } ~
+        put {
+          extractObject { (bucketName, keyName) =>
+            doPutObject(bucketName, keyName)
+          }
+        } ~
+        delete {
+          extractBucket { bucketName =>
+            doDeleteBucket(bucketName)
+          }
+        } ~
+        delete {
+          extractObject { (bucketName, keyName) =>
+            parameter("uploadId") { a =>
+              doAbortMultipartUpload(bucketName, keyName)
+            }
+          }
+        } ~
+        delete {
+          extractObject { (bucketName, keyName) =>
+            doDeleteObject(bucketName, keyName)
+          }
+        } ~
+        post {
+          extractBucket { bucketName =>
+            parameter("delete") { a =>
+              doDeleteMultipleObjects(bucketName)
+            }
+          }
+        } ~
+        post {
+          extractObject { (bucketName, keyName) =>
+            parameter("uploads") { a =>
+              doInitiateMultipartUpload(bucketName, keyName)
+            }
+          }
+        } ~
+        post {
+          extractObject { (bucketName, keyName) =>
+            parameter("uploadId") { uploadId =>
+              doCompleteMultipleUpload(bucketName, keyName, uploadId)
+            }
+          }
+        } ~
+        head {
+          extractBucket { bucketName =>
+            doHeadBucket(bucketName)
+          }
+        } ~
+        head {
+          extractObject { (bucketName, keyName) =>
+            doHeadObject(bucketName, keyName)
+          }
+        }
+  }
